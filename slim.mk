80973c9b302e199043adbf668110a5b5b2a22111
evnit <evnithn@gmail.com>
2014-06-24 20:58:23 +0700

    device slim e120l ...

---
+++ slim.mk
@@ -0,0 +1,24 @@
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/e120l/full_e120l.mk)
+
+# Inherit some common slim stuff.
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+
+# Inherit some common slim stuff.
+$(call inherit-product, vendor/slim/config/gsm.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := e120l
+
+# Device identifier
+PRODUCT_NAME := slim_e120l
+PRODUCT_DEVICE := e120l
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SHV-E120L TARGET_DEVICE=SHV-E120L BUILD_FINGERPRINT="samsung/SHV-E120L/SHV-E120L:4.4.2/KOT49H/KLJMA4:user/release-keys" PRIVATE_BUILD_DESC="SHV-E120L-user 4.4.2 KOT49H KLJMA4 release-keys"
