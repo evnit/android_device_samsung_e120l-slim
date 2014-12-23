de046ee506a7d06153c98593dcf55ae694391a16
Le Anh Dung <evnithn@gmail.com>
2014-12-23 20:06:22 +0700

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,6 +27,7 @@ TARGET_OTA_ASSERT_DEVICE := SHV-e120l,e120l
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom kgsl.mmutype=gpummu usb_id_pin_rework=true no_console_suspend=true zcache msm_watchdog.appsbark=0 msm_watchdog.enable=1 loglevel=4 androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x40400000
 TARGET_KERNEL_CONFIG := cyanogenmod_e120l_defconfig
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x41800000
 TARGET_KERNEL_SOURCE := kernel/samsung/msm8660-common
 
 # Partitions
